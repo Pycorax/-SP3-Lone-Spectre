2c60109ca99b923a1ea741b403b49a1d09ac2ce3
Kah Wei <msntkw@msn.com>
2015-08-20 10:05:02 +0800

    Improved Viewer Interface ----- - Added a SpottedTarget() to allow tiles to call for the viewer's attention

--- Base/Source/Viewer.h
+++ Base/Source/Viewer.h
@@ -1,6 +1,8 @@
 #ifndef VIEWER_H
 #define VIEWER_H
 
+#include "CChar.h"
+
 class Viewer
 {
 private:		// Variables
@@ -16,8 +18,15 @@ public:			// Functions
 	/*
 	 * Getters
 	 */
-	int GetMaxRangeX(void);
+	int GetMaxRangeX(void);	
 	int GetMaxRangeY(void);
+
+	/*
+	 *	Abstract Functions
+	 *	These functions need to be defined in child classes and serves as the interface between child classes and this class.
+	 */
+	virtual void SpottedTarget(Character* _target) = 0;		// Function to be called by the tile that stores the Viewers, to inform the Viewer of the Character that has entered the Viewer's viewing box
+
 };
 
 #endif
