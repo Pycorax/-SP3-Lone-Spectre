486336ea7a2e842f510c2e0694b58a6288feb07d
NothingMuch123 <curious_boii@hotmail.com>
2015-08-26 20:00:35 +0800

    Test commit and push ----- - Enable shadow

--- Base/Source/MVC_Model_Spectre.cpp
+++ Base/Source/MVC_Model_Spectre.cpp
@@ -5,7 +5,7 @@ MVC_Model_Spectre::MVC_Model_Spectre(string configSONFile) : MVC_Model(configSON
 	, m__currentLevel(NULL)
 	, m_hackMode(false)
 	, m__player(NULL)
-	, m_enableShadow(false)
+	, m_enableShadow(true)
 {
 }
 
